f2b1948494e4eb131531cee22822b96bf1431c47
i054267 <yuan.wang01@sap.com>
2019-01-06 01:54:37 +0800

    homework 2 16-18

--- decision_stump.py
+++ decision_stump.py
@@ -1,47 +1,51 @@
-import random
-from scipy import stats, sign
+import numpy as np
+from scipy import sign
 
 
 def sample_generator(N=20, noise=0.2):
     while True:
-        random.seed()
-        x = []
-        y = []
-        for i in range(N):
-            x.append(random.uniform(-1, 1))
-            if stats.bernoulli.rvs(noise, size=1) == 1:
-                y.append(-1 * sign(x[i]))
-            else:
-                y.append(sign(x[i]))
-        yield x, y
+        x = np.random.uniform(-1, 1, N)
+        noise_flag = np.random.binomial(1, noise, N)
+        y = sign(x)
+        y[noise_flag == 1] *= -1
+        p = x.argsort()
+        yield x[p], y[p]
 
 
-def get_e_in(theta, x, y):
+def get_e_in(theta, s, x, y):
     e_in = 0
     for i in range(len(x)):
-        if (x[i] <= theta and y[i] == 1) or (x[i] > theta and y[i] == -1):
+        if s * sign(x[i] - theta) != y[i]:
             e_in += 1 / len(x)
     return e_in
 
 
-def get_e_out(theta):
-    e_out = 0.5 + 0.3 * (abs(theta) - 1)
+def get_e_out(theta, s, noise=0.2):
+    e_out = 0.5 + (0.5-noise) * s * (abs(theta) - 1)
     return e_out
 
 
 def decision_stump(x, y):
     optimal_e_in = 1.0
     optimal_theta = 0.0
-    for i in range(len(x)+1):
-        if i == len(x):
-            theta = -1
+    for i in range(len(x)):
+        if i == 0:
+            theta = (-1 + x[i]) / 2
         else:
-            theta = x[i] + 1e-8
-        e_in = get_e_in(theta, x, y)
+            theta = (x[i-1] + x[i]) / 2
+        e_in_positive = get_e_in(theta, 1, x, y)
+        e_in_negative = get_e_in(theta, -1, x, y)
+        if e_in_positive <= e_in_negative:
+            e_in = e_in_positive
+            s = 1
+        else:
+            e_in = e_in_negative
+            s = -1
         if e_in < optimal_e_in:
             optimal_e_in = e_in
             optimal_theta = theta
-    e_out = get_e_out(optimal_theta)
+            optimal_s = s
+    e_out = get_e_out(optimal_theta, optimal_s)
     return optimal_e_in, e_out
 
 
@@ -61,8 +65,3 @@ if __name__ == '__main__':
     e_in_average, e_out_average = get_average_errors()
     print("average Ein = %.3f" % e_in_average)
     print("average Eout = %.3f" % e_out_average)
-
-
-
-
-
