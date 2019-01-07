5f8221f9ea7b8e9bd3fff393c3f6ec620cb691f8
i054267 <yuan.wang01@sap.com>
2019-01-08 00:13:17 +0800

    add __name__ == __main__

--- VC_compare_bound.py
+++ VC_compare_bound.py
@@ -1,10 +1,6 @@
 from scipy import optimize, exp, sqrt, log, power
 import matplotlib.pyplot as plt
-
-
-dvc = 50
-delta = 0.05
-N = 10000.0
+import sys
 
 
 # growth function
@@ -20,13 +16,18 @@ def devroye(epsilon):
     return sqrt((4*epsilon*(1+epsilon) + log(4*mh(N**2, dvc)/delta))/(2*N)) - epsilon
 
 
-original_vc_bound = sqrt(8*log(4*mh(2*N, dvc)/delta)/N)
-rademacher_penalty_bound = sqrt(2*log(2*N*mh(N, dvc))/N) + sqrt(2*log(1/delta)/N) + 1/N
-variant_vc_bound = sqrt(16*log(2*mh(N, dvc)/sqrt(delta))/N)
+if __name__ == '__main__':
+    dvc = 50
+    delta = 0.05
+    N = float(sys.argv[1])
 
-x_list = ["original vc", "Radem penaulty", "P&V", "Devroye", "variant vc"]
-y_list = [original_vc_bound, rademacher_penalty_bound,
-          optimize.newton(pv_bound, 0), optimize.newton(devroye, 0), variant_vc_bound]
+    original_vc_bound = sqrt(8*log(4*mh(2*N, dvc)/delta)/N)
+    rademacher_penalty_bound = sqrt(2*log(2*N*mh(N, dvc))/N) + sqrt(2*log(1/delta)/N) + 1/N
+    variant_vc_bound = sqrt(16*log(2*mh(N, dvc)/sqrt(delta))/N)
 
-plt.scatter(x_list, y_list)
-plt.show()
+    x_list = ["original vc", "Radem penaulty", "P&V", "Devroye", "variant vc"]
+    y_list = [original_vc_bound, rademacher_penalty_bound,
+              optimize.newton(pv_bound, 0), optimize.newton(devroye, 0), variant_vc_bound]
+
+    plt.scatter(x_list, y_list)
+    plt.show()
--- VC_estimate_N.py
+++ VC_estimate_N.py
@@ -1,14 +1,14 @@
 from scipy import optimize
 from scipy import exp, power
 
-epsilon = 0.05
-dvc = 10
-delta = 0.05
-
 
 def f(N):
     return 4*power(2*N, dvc)*exp(-1/8*power(epsilon, 2)*N) - delta
 
 
-root = optimize.newton(f, 420000)
-print("root = %.2f" % root)
+if __name__ == '__main__':
+    epsilon = 0.05
+    dvc = 10
+    delta = 0.05
+    root = optimize.newton(f, 420000)
+    print("root = %.2f" % root)
