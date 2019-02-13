4fe4ab1e7d3d45db656c77d5f4c2480341c4f6d7
i054267 <yuan.wang01@sap.com>
2019-02-13 11:26:56 +0100

    SVM vs. PLA N=[10, 100] M=1000

--- SVM.py
+++ SVM.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import sign
 from cvxopt import solvers, matrix
+from multiprocessing import Pool, cpu_count
 
 from linear_regression import print_sample, wbline
 from PLA import Perceptron
@@ -44,6 +45,7 @@ def svm_hard_margin_primal(x, y):
     p = matrix(0.0, (3, 1))
     G = matrix(-1 * x_plus * y[:, np.newaxis])
     h = matrix(-1.0, (N, 1))
+    solvers.options['show_progress'] = False
     sol = solvers.qp(Q, p, G, h)
 
     weight = sol['x'][1:]
@@ -62,6 +64,7 @@ def svm_hard_margin_dual(x, y):
     h = matrix(0.0, (N, 1))
     A = matrix(y[np.newaxis, :])
     b = matrix(0.0)
+    solvers.options['show_progress'] = False
     sol = solvers.qp(Q, p, G, h, A, b)
 
     alpha = np.squeeze(sol['x'])
@@ -77,8 +80,53 @@ def print_support_vector(x, y, sv):
             plt.scatter(x[i,0], x[i,1], c='w', marker='.')
 
 
-if __name__ == '__main__':
-    N = 10
+def get_error(target_w, target_b, model_w, model_b):
+    def get_x1(x0, w, b):
+        return -1 * (w[0] * x0 + b) / w[1]
+    target_x1_pos = get_x1(1, target_w, target_b)
+    target_x1_minus = get_x1(-1, target_w, target_b)
+    model_x1_pos = get_x1(1, model_w, model_b)
+    model_x1_minus = get_x1(-1, model_w, model_b)
+    error = abs(target_x1_pos - model_x1_pos) + abs(target_x1_minus - model_x1_minus)
+    return error
+
+
+def compare_pla_svm(N=10):
+    target_w, target_b = generate_target_weight_bias()
+    x, y = generate_samples(N, target_w, target_b)
+    pla_w, pla_b = get_pla_weight_bias(x, y)
+    svm_w, svm_b, sv = svm_hard_margin_dual(x, y)
+    pla_error = get_error(target_w, target_b, pla_w, pla_b)
+    svm_error = get_error(target_w, target_b, svm_w, svm_b)
+    svm_win = 0
+    if pla_error > svm_error:
+        svm_win = 1
+    return [svm_win, len(sv)]
+
+
+def get_svm_wins(N=10, M=1000):
+    svm_wins = 0.0
+    num_sv = 0.0
+    for i in range(M):
+        svm_win, sv = compare_pla_svm(N)
+        svm_wins += svm_win / M
+        num_sv += sv / M
+    return [svm_wins, num_sv]
+
+
+def get_svm_wins_multiprocessing(N=10, M=1000):
+    '''python quit unexpectedly on macos for N=100, M=1000'''
+    args = [N for i in range(M)]
+    pool = Pool(cpu_count())
+    results = pool.map(compare_pla_svm, args)
+    #a = sum(row[0] for row in results)
+    #b = sum(row[1] for row in results)
+    #return [a/M, b/M]
+    results = np.array(pool.map(compare_pla_svm, args)) # not due to numpy
+    return np.sum(results, axis=0) / M
+
+
+def unit_test(N=10):
     target_w, target_b = generate_target_weight_bias()
     x, y = generate_samples(N, target_w, target_b)
     print_sample(x, y)
@@ -87,10 +135,27 @@ if __name__ == '__main__':
     pla_w, pla_b = get_pla_weight_bias(x, y)
     wbline(pla_w, pla_b, fmt=":")
 
-    #svm_w, svm_b = svm_hard_margin_primal(x, y)
+    # svm_w, svm_b = svm_hard_margin_primal(x, y)
     svm_w, svm_b, sv = svm_hard_margin_dual(x, y)
     wbline(svm_w, svm_b, fmt="--")
     print_support_vector(x, y, sv)
-    print(sv)
+    print("support vectors:", sv)
+
+    pla_error = get_error(target_w, target_b, pla_w, pla_b)
+    svm_error = get_error(target_w, target_b, svm_w, svm_b)
+
+    print("pla_error = %.3f" % pla_error)
+    print("svm_error = %.3f" % svm_error)
+
+    print("cpu_count=", cpu_count())
 
     plt.show()
+
+
+if __name__ == '__main__':
+    #unit_test()
+
+    svm_wins_10, num_sv_10 = get_svm_wins(N=10)
+    svm_wins_100, num_sv_100 = get_svm_wins(N=100)
+    print("N=10, svm_wins=%.3f, num_sv=%.2f" % (svm_wins_10, num_sv_10))
+    print("N=100, svm_wins=%.3f, num_sv=%.2f" % (svm_wins_100, num_sv_100))
