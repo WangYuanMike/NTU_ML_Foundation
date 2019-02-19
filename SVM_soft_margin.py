d09613a3e775734ba20cdcd09326efc343ba3c23
i054267 <yuan.wang01@sap.com>
2019-02-19 16:02:07 +0100

    one vs. all

--- SVM_soft_margin.py
+++ SVM_soft_margin.py
@@ -9,9 +9,7 @@ import PLA_SVM as ps
 
 class SVM:
 
-    EPSILON = 1e-4
-
-    def __init__(self, train_x, train_y, test_x, test_y, c=1, kernel=None, q=1, zeta=0, gamma=1):
+    def __init__(self, train_x, train_y, test_x, test_y, c=1.0, kernel=None, q=1, zeta=0, gamma=1):
         """
         SVM soft margin dual problem with kernel function
         :param train_x:
@@ -44,21 +42,7 @@ class SVM:
         self.free_sv_x = None
         self.free_sv_y = None
 
-    @staticmethod
-    def load_samples(sample_file):
-        samples = np.loadtxt(sample_file)
-        x = np.ones(samples.shape)
-        x[:, 1:] = samples[:, 1:]  # x = [1, x0, x1]
-        y = samples[:, 0]
-        return x, y
-
-    @staticmethod
-    def plot_samples(x, y):
-        plt.figure(figsize=(10, 8))
-        sc = plt.scatter(x[:,1], x[:,2], s=32, c=y, cmap=cm.get_cmap('Set1'), marker='*')
-        plt.colorbar(sc)
-
-    def fit(self):
+    def fit(self, verbose=False):
         """
         - Use quadratic programming to fit the model
         - Kernel SVM fit could not provide explicit weight and bias for prediction
@@ -66,11 +50,11 @@ class SVM:
           support vectors and free support vectors for prediction
         """
         def polynomial_kernel():
-            """dot(NxD, DxN) = NxN"""
+            """(N, D) x (D, N) = (N, N)"""
             return np.power(self.zeta + self.gamma * np.dot(self.train_x, self.train_x.T), self.q)
 
         def gaussian_kernel():
-            """dot(NxD, DxN) = NxN"""
+            """(N, D) x (D, N) = (N, N)"""
             ns = np.linalg.norm(self.train_x, axis=1) ** 2
             return np.exp(-self.gamma * (ns[:, None] + ns[None, :] - 2 * np.dot(self.train_x, self.train_x.T)))
 
@@ -79,6 +63,7 @@ class SVM:
         elif self.kernel == 'g':
             k = gaussian_kernel()
 
+        epsilon = 1e-4
         N = len(self.train_y)
         Q = matrix(np.outer(self.train_y, self.train_y) * k)
         p = matrix(-1.0, (N, 1))
@@ -93,12 +78,12 @@ class SVM:
         h = matrix(h_np)
         A = matrix(self.train_y[None, :])
         b = matrix(0.0)
-        solvers.options['show_progress'] = False
+        solvers.options['show_progress'] = verbose
         sol = solvers.qp(Q, p, G, h, A, b)
 
         alpha = np.squeeze(sol['x'])
-        self.sv = np.squeeze(np.where(alpha > SVM.EPSILON))
-        self.free_sv = np.squeeze(np.where((alpha > SVM.EPSILON) & (alpha / self.c < 0.99)))
+        self.sv = np.squeeze(np.where(alpha > epsilon))
+        self.free_sv = np.squeeze(np.where((alpha > epsilon) & (alpha / self.c < 0.99)))
         self.sv_alpha = alpha[self.sv]
         self.sv_x = self.train_x[self.sv]
         self.sv_y = self.train_y[self.sv]
@@ -109,44 +94,49 @@ class SVM:
     def predict(self, x_predict):
         """
         Predict the class for sample feature x_predict
-        :param x_predict: feature vector of the predict sample (D x 1)
+        :param x_predict: feature vector of the predict sample (D,)
         :return: predict class and predict score
         """
         def polynomial_kernel(x):
-            """dot(SV_Num x D, Dx1) = SV_Num x 1"""
+            """(SV_Num, D) x (D,) = (SV_Num,)"""
             return np.power(self.zeta + self.gamma * np.dot(self.sv_x, x), self.q)
 
         def gaussian_kernel(x):
-            """dot(SV_Num x D, Dx1) = SV_Num x 1"""
+            """norm((SV_Num, D) - ((SV_Num), D), axis=1) = (SV_Num,)"""
             return np.exp(-self.gamma * np.linalg.norm(self.sv_x - x, axis=1) ** 2)
 
         def kernel_sum(x):
-            """dot(1 x SV_Num, SV_Num x 1) = 1"""
+            """(SV_Num,) x (SV_Num,) = 1"""
             if self.kernel is None or self.kernel == 'p':
                 return np.dot(self.sv_alpha * self.sv_y, polynomial_kernel(x))
             elif self.kernel == 'g':
                 return np.dot(self.sv_alpha * self.sv_y, gaussian_kernel(x))
 
-        b = self.free_sv_y[0] - kernel_sum(self.free_sv_x[0])
+        b = 0.0
+        free_sv_num = len(self.free_sv)
+        for i in range(free_sv_num):
+            b += (self.free_sv_y[i] - kernel_sum(self.free_sv_x[i])) / free_sv_num
+
         predict_score = kernel_sum(x_predict) + b
         return sign(predict_score), predict_score
 
-    def draw_decision_boundary(self, x1_low=-1.0, x1_hi=1.0, x2_low=-1.0, x2_hi=1.0, offset=1e-2):
-        x1_list = np.arange(x1_low, x1_hi, offset)
-        x2_list = np.arange(x2_low, x2_hi, offset)
-        boundary = np.zeros((x1_list.shape[0], 2))
+    def draw_decision_boundary(self, x0_low=-1.0, x0_hi=1.0, x1_low=-1.0, x1_hi=1.0, M=200):
+        x0_list = np.arange(x0_low, x0_hi, (x0_hi - x0_low) / M)
+        x1_list = np.arange(x1_low, x1_hi, (x1_hi - x1_low) / M)
+        boundary = np.zeros((x0_list.shape[0], 2))
         upper_bound = np.zeros(boundary.shape)
         lower_bound = np.zeros(boundary.shape)
 
-        for index, x1 in enumerate(x1_list):
-            for x2 in x2_list:
-                x = np.array([x1, x2])
+        epsilon = 5e-2
+        for index, x0 in enumerate(x0_list):
+            for x1 in x1_list:
+                x = np.array([x0, x1])
                 _, predict_score = self.predict(x)
-                if abs(predict_score) < offset * 2:
+                if abs(predict_score) < epsilon:
                     boundary[index] = x
-                if abs(predict_score - 1) < offset * 2:
+                if abs(predict_score - 1) < epsilon:
                     upper_bound[index] = x
-                if abs(predict_score + 1) < offset * 2:
+                if abs(predict_score + 1) < epsilon:
                     lower_bound[index] = x
 
         plt.scatter(boundary[:, 0], boundary[:, 1], s=8, marker='.')
@@ -156,6 +146,20 @@ class SVM:
     def draw_support_vector(self):
         plt.scatter(self.sv_x[:, 0], self.sv_x[:, 1], s=256, edgecolor='k', facecolor='None', marker='s')
         plt.scatter(self.free_sv_x[:, 0], self.free_sv_x[:, 1], s=256, edgecolor='k', facecolor='None', marker='*')
+
+    def get_error(self, x, y):
+        y_predict = np.zeros(len(y))
+        for i, feature in enumerate(x):
+            y_predict[i], _ = self.predict(feature)
+        error = np.zeros(y_predict.shape)
+        error[y != y_predict] = 1
+        return error.sum() / len(error)
+
+    def e_in(self):
+        return self.get_error(self.train_x, self.train_y)
+
+    def e_out(self):
+        return self.get_error(self.test_x, self.test_y)
 
     @staticmethod
     def unit_test():
@@ -168,22 +172,49 @@ class SVM:
         pla_w, pla_b = ps.get_pla_weight_bias(x, y)
         ps.wbline(pla_w, pla_b, fmt="--")
 
-        svm = SVM(x, y, x, y, c=1, kernel='p', q=2, zeta=1)
+        svm = SVM(x, y, x, y, c=0.01, kernel='p', q=2, zeta=1)
         svm.fit()
+        print("unit test E_in=%.3f" % (svm.e_in()))
         svm.draw_decision_boundary()
         svm.draw_support_vector()
-
         plt.show()
 
-        '''
-        train_file = "./features.train"
-        test_file = "./features.test"
-        train_x, train_y = load_samples(train_file)
-        test_x, test_y = load_samples(test_file)
-        plot_samples(train_x, train_y)
-        #plot_samples(test_x, test_y)
-        plt.show()
-        '''
+
+def load_samples(sample_file):
+    samples = np.loadtxt(sample_file)
+    x = samples[:, 1:]
+    y = samples[:, 0]
+    return x, y
+
+
+def plot_samples(x, y):
+    plt.figure(figsize=(10, 8))
+    sc = plt.scatter(x[:, 0], x[:, 1], s=32, c=y, cmap=cm.get_cmap('Set1'), marker='*')
+    plt.colorbar(sc)
+
+
+def ova_transform(y, i):
+    y_ova = y.copy()
+    y_ova[y_ova != i] = -1
+    return y_ova
+
 
 if __name__ == '__main__':
-    SVM.unit_test()
+    #SVM.unit_test()
+
+    train_file = "./features.train"
+    test_file = "./features.test"
+    train_x, train_y = load_samples(train_file)
+    test_x, test_y = load_samples(test_file)
+    #plot_samples(train_x, train_y)
+
+    # one vs.all experiments
+    c = 0.01
+    k = 'p'
+    q = 2
+    z = 1
+    for i in range(10):
+        svm = SVM(train_x, ova_transform(train_y, i), test_x, ova_transform(test_y, i), c=c, kernel=k, q=q, zeta=z)
+        svm.fit(verbose=False)
+        print("c=%.2e, kernel=%s, q=%d ,zeta=%d | %d vs. all | E_in=%.3f, E_out=%.3f sv_num=%d" %
+              (c, k, q, z, i, svm.e_in(), svm.e_out(), len(svm.sv)))
