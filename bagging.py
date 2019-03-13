541739792ec6c97e04b5a7d840119c5b41d51863
i054267 <yuan.wang01@sap.com>
2019-03-14 00:35:16 +0100

    bagging

---
+++ bagging.py
@@ -0,0 +1,50 @@
+import kernel_ridge_regression as krr
+import numpy as np
+
+
+def bootstrap(x, y):
+    index = list(range(len(x)))
+    a = np.random.choice(index, len(x), replace=True)
+    return x[a], y[a]
+
+
+def bagging(T, lamb, train_x, train_y, test_x, test_y):
+    total_y_in = np.zeros(len(train_y))
+    total_y_out = np.zeros(len(test_y))
+    for t in range(T):
+        boot_train_x, boot_train_y = bootstrap(train_x, train_y)
+        model = krr.KernelRidgeRegression(kernel='l', lamb=lamb)
+        model.fit(boot_train_x, boot_train_y)
+        predict_y_in, _ = model.predict(boot_train_x, boot_train_y)
+        predict_y_out, _ = model.predict(test_x, test_y)
+        total_y_in += predict_y_in
+        total_y_out += predict_y_out
+    bagging_y_in = np.sign(total_y_in)
+    bagging_y_out = np.sign(total_y_out)
+    return bagging_y_in, bagging_y_out, krr.get_error(boot_train_y, bagging_y_in), krr.get_error(test_y, bagging_y_out)
+
+
+def bagging_krr_experiment(T):
+    x, y = krr.load_samples("./hw2_lssvm_all.dat")
+    train_x = x[:400, :]
+    train_y = y[:400]
+    test_x = x[400:, :]
+    test_y = y[400:]
+
+    lamb_list = [1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10]
+    e_in_list = np.zeros(len(lamb_list))
+    e_out_list = np.zeros(e_in_list.shape)
+    for i, lamb in enumerate(lamb_list):
+        _, _, e_in, e_out = bagging(T, lamb, train_x, train_y, test_x, test_y)
+        e_in_list[i] = e_in
+        e_out_list[i] = e_out
+
+    print("e_in_list:")
+    print(e_in_list)
+    print("e_out_list")
+    print(e_out_list)
+
+
+if __name__ == '__main__':
+    krr.linear_krr_experiment()
+    bagging_krr_experiment(250)
--- kernel_ridge_regression.py
+++ kernel_ridge_regression.py
@@ -3,27 +3,44 @@ from sklearn_SVM import get_error
 
 
 class KernelRidgeRegression():
-    def __init__(self, gamma=1.0, lamb=1.0):
+    def __init__(self, kernel='g', gamma=1.0, lamb=1.0):
+        self.kernel = kernel
         self.gamma = gamma
         self.lamb = lamb
         self.beta = None
         self.train_x = None
 
     def fit(self, x, y):
+        def linear_kernel():
+            return np.dot(x, x.T)
+
         def rbf_kernel():
             ns = np.linalg.norm(x, axis=1) ** 2
             return np.exp(-self.gamma * (ns[:, None] + ns[None, :] - 2 * np.dot(x, x.T)))
 
-        self.beta = np.dot(np.linalg.inv(self.lamb * np.identity(len(y)) + rbf_kernel()), y)
+        if self.kernel == 'g':
+            k = rbf_kernel()
+        else:
+            k = linear_kernel()
+
+        self.beta = np.dot(np.linalg.inv(self.lamb * np.identity(len(y)) + k), y)
         self.train_x = x
 
     def predict(self, x, y):
+        def linear_kernel():
+            return np.dot(x, self.train_x.T)
+
         def rbf_kernel():
             return np.exp(-self.gamma * np.linalg.norm(self.train_x[None, :, :] - x[:, None, :], axis=2) ** 2)
 
-        predict_y = np.sign(np.dot(rbf_kernel(), self.beta))
-        print(predict_y)
-        print(y)
+        if self.kernel == 'g':
+            k = rbf_kernel()
+        else:
+            k = linear_kernel()
+
+        predict_y = np.sign(np.dot(k, self.beta))
+        #print(predict_y)
+        #print(y)
         error = get_error(y, predict_y)
         return predict_y, error
 
@@ -61,5 +78,30 @@ def krr_experiment():
     print(e_out_list)
 
 
+def linear_krr_experiment():
+    x, y = load_samples("./hw2_lssvm_all.dat")
+    train_x = x[:400, :]
+    train_y = y[:400]
+    test_x = x[400:, :]
+    test_y = y[400:]
+
+    lamb_list = [1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10]
+    e_in_list = np.zeros(len(lamb_list))
+    e_out_list = np.zeros(e_in_list.shape)
+    for i, lamb in enumerate(lamb_list):
+        model = KernelRidgeRegression(kernel='l', lamb=lamb)
+        model.fit(train_x, train_y)
+        _, e_in = model.predict(train_x, train_y)
+        _, e_out = model.predict(test_x, test_y)
+        e_in_list[i] = e_in
+        e_out_list[i] = e_out
+
+    print("e_in_list:")
+    print(e_in_list)
+    print("e_out_list")
+    print(e_out_list)
+
+
 if __name__ == '__main__':
-    krr_experiment()
+    #krr_experiment()
+    linear_krr_experiment()
